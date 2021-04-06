624729534d95ecd270f9ba8402ff2727cc8d98b3
Lagaet Jeroen <jerlag_01@outlook.com>
2021-04-06 19:40:19 +0200

    Update connection
    Updated connection

--- required/connection.php
+++ required/connection.php
@@ -13,4 +13,14 @@ print $conn->error;
 if (mysqli_connect_error()) {
 	echo "Failed to connect to Database: $database" . mysqli_connect_error();
 }
+
+$site = "SELECT sitename FROM settings";
+$getsite = mysqli_query($conn, $site);
+
+if(! $getsite) {
+	die('Could not fetch data: '.mysqli_error($conn));
+}
+while($row = mysqli_fetch_assoc($getsite)){
+	$sitename = htmlspecialchars($row['sitename']);
+}
 ?>
