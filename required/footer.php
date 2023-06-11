766c67c1f23f8c9ceb1cba9a549570a8ef4bf181
Lagaet Jeroen <jerlag@jl-tech.be>
2023-06-12 00:37:35 +0200

    reset

--- required/footer.php
+++
@@ -1,4 +0,0 @@
-Copyright &copy; <a href="https://www.powerinstallation.be">Power Installation</a> 1998 - <?php echo date('Y'); ?>. All Rights Reserved
-<div class="pull-right small text-gray" style="margin-right:10px;margin-top:-7px;">
-	<strong>Contact: support@powerinstalllation.be</strong>
-</div>
